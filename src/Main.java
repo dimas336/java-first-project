38b08110db016f05518ebc47f10e3b9ae1593b56
Dima <dmarshev2004@icloud.com>
2022-11-20 19:45:13 +0300

    first_commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=48;
+        int c=-34;
+        int d=12;
+
+        int max1;
+        int max2;
+
+        if (a<b){
+            max1=b;
+        }
+        else{
+            max1=a;
+        }
+
+        if (c<d){
+            max2=d;
+        }
+        else{
+            max2=c;
+        }
+
+        if (max1<max2){
+            System.out.println(max2);
+        }
+        else{
+            System.out.println(max1);
+        }
+    }
+}
